cb6097b3be1dc3473797bd9c959d4deb19fe26fa
Shivangi <shivangiyadav112@gmail.com>
2023-09-30 21:59:08 +0530

    S

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {},
+    fontFamily:{
+      poppins:["Poppins", "sans-serif"],
+      Archivo:["Archivo Black" , "sans-serif"],
+      Roboto:["Roboto" , "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
